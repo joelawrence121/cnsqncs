03f8d60f865dd5ab6b6413d065aa305feaf46559
Joe Lawrence <joelawrence121@hotmail.co.uk>
2021-12-26 18:35:41 +0000

    Add initial backend

---
+++ cnsq_server.py
@@ -0,0 +1,72 @@
+import logging
+
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+from consequences_service import ConsequencesService
+from objects.api import CreateRequest, JoinRequest, PollRequest, EntryRequest
+
+app = FastAPI()
+consequences_service = ConsequencesService()
+
+ORIGINS = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+FORMAT = '%(asctime)-15s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('cnsq_server')
+
+
+@app.post("/create")
+async def create_game(request: CreateRequest):
+    try:
+        return consequences_service.create_game(request.name)
+    except RuntimeError as e:
+        logger.warning(e)
+
+
+@app.post("/join")
+async def join_game(request: JoinRequest):
+    try:
+        return consequences_service.join_game(request.game_id, request.name)
+    except RuntimeError as e:
+        logger.warning(e)
+
+
+@app.get("/poll")
+async def poll_game(request: PollRequest):
+    try:
+        return consequences_service.poll_game(request.name, request.game_id)
+    except RuntimeError as e:
+        logger.warning(e)
+
+
+@app.get("/start_game/{game_id}")
+async def start_game(game_id):
+    try:
+        return consequences_service.start_game(game_id)
+    except RuntimeError as e:
+        logger.warning(e)
+
+
+@app.post("/post_entry")
+async def post_entry(request: EntryRequest):
+    try:
+        return consequences_service.post_entry(request)
+    except RuntimeError as e:
+        logger.warning(e)
+
+
+if __name__ == "__main__":
+    uvicorn.run("cnsq_server:app", host="127.0.0.1", port=5000, log_level="info", workers=1)
---
+++ objects/domain.py
@@ -0,0 +1,101 @@
+import uuid
+from enum import Enum
+
+
+class Player:
+    def __init__(self, name: str, creator: bool):
+        self.name = name
+        self.creator = creator
+
+
+class StoryState(Enum):
+    NOT_STARTED = "<ready to start>"
+    MAN = "(man's name)"
+    WOMAN = "met (woman's name)"
+    MET = 'in / at'
+    HE_WORE = 'he wore'
+    SHE_WORE = 'she wore'
+    HE_SAID = 'he said'
+    SHE_SAID = 'she said'
+    CONSEQUENCE = 'and the consequence was'
+    FINISHED = "<finished>"
+
+
+class Entry:
+    def __init__(self, player: Player, entry: str):
+        self.player = player
+        self.entry = entry
+
+
+class Story:
+    STATE_SEQUENCE = [
+        StoryState.NOT_STARTED,
+        StoryState.MAN,
+        StoryState.WOMAN,
+        StoryState.MET,
+        StoryState.HE_WORE,
+        StoryState.SHE_WORE,
+        StoryState.HE_SAID,
+        StoryState.SHE_SAID,
+        StoryState.CONSEQUENCE,
+        StoryState.FINISHED
+    ]
+
+    def __init__(self, player):
+        self.state = StoryState.NOT_STARTED
+        self.ENTRIES = dict.fromkeys(self.STATE_SEQUENCE)
+        self.current_player = player
+
+    def post_entry(self, player: Player, entry: str):
+        self.state = self.STATE_SEQUENCE[(self.STATE_SEQUENCE.index(self.state) + 1) % len(self.STATE_SEQUENCE)]
+        self.ENTRIES[self.state] = Entry(player, entry)
+
+
+class GameState(Enum):
+    NOT_STARTED = "<not started>"
+    WAITING = "<waiting>"
+    FINISHED = "<finished>"
+
+
+class Game:
+    def __init__(self, player_name):
+        self.id = uuid.uuid4().__str__()[:3]
+        self.game_state = GameState.NOT_STARTED
+        self.story_state = StoryState.MAN
+        self.players = [Player(player_name, True)]
+        self.stories = []
+
+    def get_waiting_for(self):
+        return set([story.current_player for story in self.stories if story.state != self.story_state])
+
+    def get_players(self):
+        return [player.name for player in self.players]
+
+    def player_join(self, player_name):
+        self.players.append(Player(player_name, False))
+
+    def poll_game(self, player_name):
+        waiting = [self.get_waiting_for()]
+
+        # if round is done
+        if len(waiting[0]) == 0:
+            self.story_state = Story.STATE_SEQUENCE[
+                (Story.STATE_SEQUENCE.index(self.story_state) + 1) % len(Story.STATE_SEQUENCE)]
+
+            players = [player.name for player in self.players]
+
+            for i in range(0, len(players)):
+                self.stories[i].current_player = players[
+                    (players.index(self.stories[i].current_player) + 1) % len(players)]
+
+        return [story for story in self.stories if story.current_player == player_name]
+
+    def start(self):
+        self.game_state = GameState.WAITING
+        for i in range(0, len(self.players)):
+            self.stories.append(Story(self.players[i].name))
+
+    def post_entry(self, player_name, entry):
+        story = [story for story in self.stories if story.current_player == player_name][0]
+        player = [player for player in self.players if player.name == player_name][0]
+        story.post_entry(player, entry)
