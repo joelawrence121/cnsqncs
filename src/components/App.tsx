9a104a2d1cc8d160a43d82c2754a7cf782c36f36
Joe Lawrence <joelawrence121@hotmail.co.uk>
2022-01-31 17:51:57 +0000

    v3: Add simple navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Consequences from "./Consequences";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ClearScreen from "./ClearScreen";
+import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 function getParticles(amount: number) {
     let particleList: JSX.Element[] = []
@@ -26,6 +27,18 @@ function App() {
                     <div className="App">
                         <div id="particle-container">
                             {getParticles(30)}
+                            <Navbar bg="light" expand="lg" className={"transparent"}>
+                                <Container>
+                                    <Navbar.Brand href="/">Consequences</Navbar.Brand>
+                                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                    <Navbar.Collapse id="basic-navbar-nav">
+                                        <Nav className="me-auto">
+                                            <Nav.Link href="/">Home</Nav.Link>
+                                            <Nav.Link href="/">About</Nav.Link>
+                                        </Nav>
+                                    </Navbar.Collapse>
+                                </Container>
+                            </Navbar>
                             <Consequences/>
                         </div>
                     </div>
