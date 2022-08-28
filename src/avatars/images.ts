18d79ddf53ca1ce2c12d5c508331c9a1064c3c9b
Joe Lawrence <joelawrence121@hotmail.co.uk>
2022-08-28 12:33:36 +0100

    v4: Avatar changes

--- src/avatars/images.ts
+++ src/avatars/images.ts
@@ -1,20 +1,16 @@
-import azy from './azy.png';
 import a1 from './a1.jpeg';
-import brune from './brune.png';
-import c1 from './c1.jpeg';
 import a2 from './a2.jpg';
 import cate from './cate.png';
 import a3 from './a3.jpeg';
 import kanz from './kanz.png';
-import oran from './oran.png';
 import rat from './rat.jpg';
 import s1 from './s1.jpg';
 import g1 from './g1.jpeg';
 import l1 from './l1.jpeg';
-import cc from './cc.jpeg';
 import sun from './sun.jpeg';
 import p1 from './p1.jpeg';
+import chugs from './chugs.jpg';
 
 export default [
-    azy, a1, brune, c1, a2, cate, a3, kanz, oran, rat, s1, g1, l1, cc, sun, p1
+    a1, a2, cate, a3, kanz, rat, s1, g1, l1, sun, p1, chugs
 ]
